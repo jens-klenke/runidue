70c31536cf5c43b89b584290d5e77126bdaa995f
Martin Schmelzer <schmelzer.martin@gmx.de>
2019-02-18 15:53:57 +0100

    Added new 2Do and reworked setup.R

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A Beamer Template for the University of Duisburg-Essen.
 ToDos:
 
 - Add LaTeX command for gicing scriptsize sources: \scriptsize Quelle: Yahoo Finance -> \source{Quelle: Yahoo Finance}
+- Inline highlighting?
--- inst/rmarkdown/templates/Beamer/skeleton/includes/r/setup.R
+++ inst/rmarkdown/templates/Beamer/skeleton/includes/r/setup.R
@@ -10,7 +10,7 @@ due.col <- list(blue = "#004c93",
 
 ### Options -------------------------------------------------------------------
 options(tikzDocumentDeclaration = '\\documentclass{beamer}')
-opts_chunk$set(echo = F, fontsize = "\\footnotesize", dev = "tikz", cache = F, message = FALSE, warning = FALSE,
+opts_chunk$set(echo = T, fontsize = "\\footnotesize", dev = "tikz", cache = T, message = FALSE, warning = FALSE,
                fig.width = 0.9, fig.asp = 0.66, fig.align = "center", c.par = T,
                fig.path = "Plots/")
 options(digits = 4)
@@ -18,14 +18,18 @@ options(digits = 4)
 ### Default plotting parameters -----------------------------------------------
 custom.par <- function(...) {
   par(mar = c(4.1, 3.1, 1.5, 1), oma = c(0, 0, 0, 0), pch = 16,
-      mgp = c(1.7, 0.4, 0), cex = 0.75,
-      tcl = -0.33, ...)
+      mgp = c(1.7, 0.4, 0), tcl = -0.33, ...)
 }
+
+# Auto set cex parameter
+fwidth  <- c(0.5, 0.6, 0.7, 0.8, 0.9, 1)
+cex     <- c(0.6, 0.7, 0.75, 0.8, 0.85, 0.9)
+magnify <- approxfun(fwidth, cex, yleft = 0.5, yright = 0.9)
 
 ### Use blue as the default plotting color
 plot <- function(..., col = NULL, pch = NULL) {
   graphics::plot(..., bty = "o",
-                 col = ifelse(is.null(col), due.col$blue, col),
+                 col = ifelse(is.null(col), "steelblue3", col),
                  pch = ifelse(is.null(pch), 16, pch))
 }
 
@@ -43,23 +47,22 @@ knitr::knit_hooks$set(output = function(x, options) {
          paste0("\\begin{Shaded}\n\\begin{verbatim}\n", gsub(pattern = "```|^\\s*$", "", x, perl = T), "\\end{verbatim}\n\\end{Shaded}"))
 })
 
-knit_hooks$set(c.par = function(before, options, envir) {
-  if (before) {
-    fwidth <- c(0.5, 0.6, 0.7, 0.8, 0.9, 1)
-    cex    <- c(0.6, 0.7, 0.75, 0.8, 0.85, 0.9)
-    magnify <- approxfun(x, y, yleft = 0.5)
-  }
-})
-
-
 opts_hooks$set(fig.width = function(options) {
   if (options$fig.width <= 1 & options$fig.width > 0) {
+    options$cex <- magnify(options$fig.width)
     options$fig.width  <- options$fig.width * slide.width
     options$fig.height <- options$fig.asp * options$fig.width
+    
   }
   options
 })
 
+knit_hooks$set(c.par = function(before, options, envir) {
+  if (before) {
+    message(paste0("---------------- ",options$cex, " ----------------"))
+    custom.par(cex = options$cex)
+  }
+})
 
 
 
