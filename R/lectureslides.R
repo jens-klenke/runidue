91d291cb9632180444e7cbf92a533eaaeddd7166
Martin Schmelzer <schmelzer.martin@gmx.de>
2019-02-24 21:52:52 +0100

    Incorporated setup.R inside lectureslides.R

--- R/lectureslides.R
+++ R/lectureslides.R
@@ -13,36 +13,159 @@
 #' @examples
 #' \dontrun{ library(rmarkdown) draft("MyLecture.Rmd", template = "lectureslides",
 #' package = "rmemo") }
-#' @importFrom rmarkdown pdf_document pandoc_path_arg pandoc_variable_arg
+#' @import rmarkdown
 #' @export
-lectureslides <- function(..., 
-                          lang = "en",
-                          towers = TRUE) {
+lectureslides <- function(lang = "en",
+                          towers = T,
+                          template = "default",
+                          toc = TRUE,
+                          slide_level = 2,
+                          incremental = FALSE,
+                          fig_width = 10,
+                          fig_height = 7,
+                          fig_crop = TRUE,
+                          fig_caption = TRUE,
+                          dev = 'pdf',
+                          df_print = "default",
+                          theme = "default",
+                          colortheme = "default",
+                          fonttheme = "default",
+                          highlight = "default",
+                          keep_tex = FALSE,
+                          latex_engine = "pdflatex",
+                          citation_package = c("none", "natbib", "biblatex"),
+                          self_contained = TRUE,
+                          includes = NULL,
+                          md_extensions = NULL,
+                          pandoc_args = NULL) {
+  
+  # base pandoc options for all beamer output
+  args <- c()
+  
+  # template path and assets
+  if (!is.null(template)) {
+    if (identical(template, "default")) template <- find_resource("lectureslides", "template.tex")
+    if (!is.null(template))
+      args <- c(args, "--template", pandoc_path_arg(template))
+  }
+  
+  # table of contents
+  if (toc)
+    args <- c(args, "--table-of-contents")
+  
+  # slide level
+  if (!is.null(slide_level))
+    args <- c(args, "--slide-level", as.character(slide_level))
+  
+  # incremental
+  if (incremental)
+    args <- c(args, "--incremental")
+  
+  # themes
+  if (!identical(theme, "default"))
+    args <- c(args, pandoc_variable_arg("theme", theme))
+  if (!identical(colortheme, "default"))
+    args <- c(args, pandoc_variable_arg("colortheme", colortheme))
+  if (!identical(fonttheme, "default"))
+    args <- c(args, pandoc_variable_arg("fonttheme", fonttheme))
+  
+  # highlighting
+  if (!is.null(highlight))
+    highlight <- match.arg(highlight, highlighters())
+  args <- c(args, pandoc_highlight_args(highlight))
+  
+  # latex engine
+  latex_engine = match.arg(latex_engine, c("pdflatex", "lualatex", "xelatex"))
+  args <- c(args, pandoc_latex_engine_args(latex_engine))
+  
+  # citation package
+  citation_package <- match.arg(citation_package)
+  if (citation_package != "none") args <- c(args, paste0("--", citation_package))
+  
+  # generate a self-contained LaTeX document (including preamble)
+  if (self_contained) args <- c(args, "--self-contained")
+  
+  # content includes
+  args <- c(args, includes_to_pandoc_args(includes))
+  
+  # make sure the graphics package is always loaded
+  if (identical(template, "default")) args <- c(args, "--variable", "graphics=yes")
+  
+  # custom args
+  args <- c(args, pandoc_args)
+  
+  # initialize saved files dir
+  saved_files_dir <- NULL
+  
+  ######
   res_path <- find_res_path("lectureslides")
   
-  # handle arguments
-  args <- c()
   args <- c(args, c(args, pandoc_variable_arg("resources", res_path)))
-  
   
   args <- c(args, pandoc_variable_arg("lang", lang))
   args <- c(args, pandoc_variable_arg("towers", towers))
   
   
+  pre_knit <- function(input, ...) {
+    options(tikzDocumentDeclaration = "\\documentclass{beamer}")
+    options(tikzMetricsDictionary="Resources/tikzDictionary")
+    options(digits = 4)
+    ### Use blue as the default plotting color
+    plot <- function(..., col = NULL, pch = NULL) {
+      graphics::plot(..., bty = "o",
+                     col = ifelse(is.null(col), "steelblue3", col),
+                     pch = ifelse(is.null(pch), 16, pch))
+    }
+  }
   
-  rmarkdown::beamer_presentation(...,
-                                 template = find_resource("lectureslides", "template.tex"), 
-                                 pandoc_args = args,
-                                 dev = "tikz",
-                                 fig_crop = T)
+  pre_processor <- function(metadata, input_file, runtime, knit_meta,
+                            files_dir, output_dir) {
+    # save files dir (for generating intermediates)
+    saved_files_dir <<- files_dir
+    base_path <<- paste0(input_file, "_resources/")
+
+    # no-op other than caching dir location
+    invisible(NULL)
+  }
+  
+  # generate intermediates (required to make resources available for publish)
+  intermediates_generator <- function(...) {
+    general_intermediates_generator(saved_files_dir, ...)
+  }
+  
+  # default knitr options
+  default_chunk_opts <- list(echo = T, 
+                             fontsize = "\\footnotesize", 
+                             dev = "tikz", 
+                             cache = T, 
+                             message = FALSE, 
+                             warning = FALSE,
+                             fig.width = 0.9,
+                             fig.asp = 0.66, 
+                             fig.align = "center", 
+                             cpar = T,
+                             fig.path = "Resources/Plots/",
+                             cache.path = "Resources/Cache/")
+  
+  # return format
+  output_format(
+    knitr = knitr_options(opts_chunk = default_chunk_opts,
+                          knit_hooks = list(chunk = chunk_hook,
+                                            output = output_hook,
+                                            cpar = custom_par_hook),
+                          opts_hooks = list(fig.width = fig_width_hook)),
+    pandoc = pandoc_options(to = "beamer",
+                            from = from_rmarkdown(fig_caption, md_extensions),
+                            args = args,
+                            latex_engine = latex_engine,
+                            keep_tex = keep_tex),
+    pre_knit = pre_knit,
+    pre_processor = pre_processor,
+    intermediates_generator = intermediates_generator,
+    clean_supporting = !keep_tex,
+    df_print = df_print
+  )
 }
-
-
-
-
-
-
-
 
 
 
--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,58 @@
+slide.width  <- 5.04 # slide width in inches
+slide.height <- 3.78
+
+# Auto set cex parameter
+fwidth  <- c(0.5, 0.6, 0.7, 0.8, 0.9, 1)
+cex     <- c(0.6, 0.7, 0.75, 0.8, 0.85, 0.9)
+magnify <- approxfun(fwidth, cex, yleft = 0.6, yright = 0.9)
+
+due.col <- list(blue = "#004c93",
+                lightblue = "#dfe4f2",
+                beige = "#efe4bf")
+
+custom_par <- function(...) {
+  par(mar = c(4.1, 3.1, 1.5, 1), oma = c(0, 0, 0, 0), pch = 16,
+      mgp = c(1.7, 0.4, 0), tcl = -0.33, ...)
+}
+
+### Hooks ---------------------------------------------------------------------
+def_hooks  <- knitr::knit_hooks$get()
+chunk_hook <- function(x, options) {
+  x <- def_hooks$chunk(x, options)
+  ifelse(!is.null(options$fontsize), paste0("\n", options$fontsize,"\n\n", x, "\n\n \\normalsize"), x)
+}
+
+output_hook <- function(x, options) {
+  ifelse(is.null(options$shadeoutput),
+         def_hooks$output(x, options),
+         paste0("\\begin{Shaded}\n\\begin{verbatim}\n", gsub(pattern = "```|^\\s*$", "", x, perl = T), "\\end{verbatim}\n\\end{Shaded}"))
+}
+
+ 
+fig_width_hook <- function(options) {
+  if (options$fig.width <= 1 & options$fig.width > 0) {
+    options$cex <- magnify(options$fig.width)
+    options$fig.width  <- options$fig.width * slide.width
+    options$fig.height <- options$fig.asp * options$fig.width
+  }
+  options
+}
+
+custom_par_hook <- function(before, options, envir) {
+  if (before) {
+    message(options$cex)
+    custom_par(cex = options$cex)
+  }
+}
+
+
+# utility functions for the rendering process -----------------------------
 find_file <- function(template, file) {
   path <- system.file("rmarkdown", "templates", template, file,
                           package = "runidue")
   if (path == "") {
     stop("Couldn't find template file ", template, "/", file, call. = FALSE)
   }
-  
  path
 }
 
@@ -14,4 +62,15 @@ find_resource <- function(template, file) {
 
 find_res_path <- function(template) {
   system.file("rmarkdown", "templates", template, "resources", package = "runidue")  
+}
+
+highlighters <- function() {
+  c("default",
+    "tango",
+    "pygments",
+    "kate",
+    "monochrome",
+    "espresso",
+    "zenburn",
+    "haddock")
 }
